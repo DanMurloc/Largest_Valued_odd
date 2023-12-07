18619404eb0d5f6ea6ba0726d1d275c8eccb0f89
DanMurloc <strokalov1@gmail.com>
2023-12-07 13:16:43 +0300

    New Solution2

--- Largest-valued odd/Program.cs
+++ Largest-valued odd/Program.cs
@@ -6,7 +6,7 @@ string num1 = "10133890";
 string num2 = "7542351161";
 string num3 = "4206";
 string num4 = "2928327648";
-string num = "239537672423884969653287101";
-Solution a  = new Solution();
+string num = "239537672423884969653287101"; //Sulution 2
+Solution2 a  = new Solution2();
 num=a.LargestOddNumber(num);
 Console.WriteLine(num);
---
+++ Largest-valued odd/Solution2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_valued_odd
+{
+    internal class Solution2
+    {
+        public string LargestOddNumber(string num)
+        {
+            Sl(ref num);
+            return num;
+        }
+        public void Sl(ref string num)
+        {
+            char[] c = num.ToCharArray();
+            while (c.Length!=0)
+            {
+                if (int.Parse(c[c.Length - 1].ToString()) % 2 != 0)
+                {
+                    num = new string(c);
+                    break;
+                }
+                else
+                {
+                    RemoveLast(ref c);
+                    num = "";
+                }
+            }
+
+           
+        }
+
+        public void RemoveAt<T>(ref T[] arr, int index)
+        {
+            T[] newArray = new T[arr.Length - 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = arr[i];
+            }
+            for (int i = index + 1; i < arr.Length; i++)
+            {
+                newArray[i - 1] = arr[i];
+            }
+            arr = newArray;
+        }
+        public void RemoveLast<T>(ref T[] array)
+        {
+            RemoveAt<T>(ref array, array.Length - 1);
+        }
+
+    }
+}
